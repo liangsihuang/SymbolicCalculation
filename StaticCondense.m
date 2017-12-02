6a202c09f91e8b0844dfd3303385e83a8c1744c9
lyoshi <380036805@qq.com>
2017-12-02 21:44:38 +0800

    StaticCondense

---
+++ StaticCondense.m
@@ -0,0 +1,10 @@
+% static condense
+L=sym('L');
+kt0=[6/L 0 6/L;-6/L 0 6/L];
+% k0t=kt0'是共轭转置; 结果里会有conj(L)，共轭
+k0t=kt0.';
+k00=[4 1 0;1 4 1;0 1 4];
+ktt=[12/L^2 0;0 12/L^2];
+k_hat=ktt-kt0*inv(k00)*k0t;
+% 最后结果是负刚度？？？
+
