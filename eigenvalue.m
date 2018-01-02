a75d33537f8d51c10cff0b5733a7622cc0a83542
lyoshi <380036805@qq.com>
2018-01-02 20:21:25 +0800

    add

--- eigenvalue.m
+++ eigenvalue.m
@@ -1,8 +1,8 @@
 clear;
 clc;
 % solve cubic equation
-% lambda=sym('lambda');
-% answer=solve('lambda^3-10*lambda^2+25*lambda-12=0');
+lambda=sym('lambda');
+answer=solve('lambda^3-10*lambda^2+25*lambda-12=0');
 
 % lambda=sym('lambda');
 % answer=solve('25*lambda^3-147*lambda^2+234*lambda-54=0');
@@ -18,5 +18,10 @@ clc;
 %    0 -6+22*lambda 2+3*lambda 4-4*lambda];
 % det(A)
 
-lambda=sym('lambda');
-answer=solve('367696*lambda^4 - 2440368*lambda^3 + 1872336*lambda^2 - 50112*lambda + 144=0');
+% 这种新格式反而解不出来？
+% syms lambda
+% equ=367696*lambda^4 - 2440368*lambda^3 + 1872336*lambda^2 - 50112*lambda + 144==0;
+% solve(equ,lambda);
+% 运行过新格式后旧格式也用不了？？
+% lambda=sym('lambda');
+% answer=solve('367696*lambda^4 - 2440368*lambda^3 + 1872336*lambda^2 - 50112*lambda + 144=0');
